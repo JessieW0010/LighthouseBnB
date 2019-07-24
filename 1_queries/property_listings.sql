92002bf07d73237fb0fa39ade912445718522f7f
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-07-24 15:16:33 -0400

    added query which lists property listing and average rating for properties in vancouver with an average rating of 4 or higher

---
+++ 1_queries/property_listings.sql
@@ -0,0 +1,3 @@
+-- When users come to our home page, they should see a list of properties. They wil be able to view the properties and filter them by location. They will be able to see all data about the property, including the average rating. 
+
+SELECT id, title, cost_per_night, average_rating
